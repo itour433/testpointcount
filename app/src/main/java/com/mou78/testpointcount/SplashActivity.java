710adc9e2f179bf212abb52f705651edbd9ee613
ryotaro <itour433@yahoo.co.jp>
2020-12-29 15:34:04 +0900

    Splash画面を実装した

---
+++ app/src/main/java/com/mou78/testpointcount/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.mou78.testpointcount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //タイトルバーを非表示
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            // Runnable型のインスタンス化と定義
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000); // 遅らせたい時間(ミリ秒) 2000ミリ秒 -> 2秒
+    }
+}
